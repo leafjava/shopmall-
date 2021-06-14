9c2ccf2e391dfa7da8e8c080e377638b157ba80b
DESKTOP-HHM2QH3\ASUS <a894074295@126.com>
2021-06-14 18:55:02 +0800

    P205  详情页不能滚动的bug处理,主要就是因为scrollHeight的问题，没有重新刷新，现在解决这个问题

--- src/common/mixin.js
+++ src/common/mixin.js
@@ -3,13 +3,14 @@ import {debounce} from "./utils";
 export const itemListenerMixin = {
   data() {
     return {
-      itemImgListener: null
+      itemImgListener: null,
+      refresh: null
     }
   },
   mounted() {
-    let refresh = debounce(this.$refs.scroll.refresh, 100)
+    this.refresh = debounce(this.$refs.scroll.refresh, 100)
 
-    this.itemImgListener = () => {refresh()}
+    this.itemImgListener = () => {this.refresh()}
 
     this.$bus.$on('itemImgLoad', this.itemImgListener)
     console.log('我是混入中的内容');
